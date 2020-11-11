99500a9872ce201a6a7c6426f54228122d07c39e
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-11-11 19:59:51 +0100

    LiveData & ViewModel

--- app/src/main/java/ika/narucivanje/MainActivity.java
+++ app/src/main/java/ika/narucivanje/MainActivity.java
@@ -3,8 +3,10 @@ package ika.narucivanje;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "activity-lifecycle";
 
     private ActivityMainBinding activityMainBinding;
+    private  MyViewModel myViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,23 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
 
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        /*
+        * Dohvatamo LiveData objekat quantity koji je svestan Life-Cyclea
+        * Kacimo observer na njega, i prosledjujemo zivotni ciklus objekta koji ga posmatra (observuje)
+        * Nakon toga definisemo sta se desava onChanged
+         */
+        myViewModel.getQuantity().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                activityMainBinding.textView.setText("Kolicina: " + integer);
+            }
+        });
+
         activityMainBinding.button.setOnClickListener(view -> {
-            activityMainBinding.textView.setText("Promenjeno nakon klika iz ClickListenera");
+            int quantity = Integer.parseInt(activityMainBinding.editText.getText().toString());
+            myViewModel.setQuantity(quantity);
         });
 
     }
--- app/src/main/java/ika/narucivanje/MyViewModel.java
+++ app/src/main/java/ika/narucivanje/MyViewModel.java
@@ -1,5 +1,23 @@
 package ika.narucivanje;
 
-public class MyViewModel {
-    private String test = "Test";
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+
+    /*
+    * MutableLiveData je samo potklasa apstraktne klase LiveData
+    * Koristimo je da ne bismo morali da definisemo sve metode LiveData klase
+     */
+    private final MutableLiveData<Integer> quantity = new MutableLiveData<>();
+
+    public MutableLiveData<Integer> getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int value) {
+        quantity.setValue(value);
+    }
+
+
 }
