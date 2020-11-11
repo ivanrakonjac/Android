f06a3e97985d0a6c60187f71cb7e43ad47c31dfc
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-11-11 18:32:42 +0100

    Activity-lifecycle methods

--- app/src/main/java/ika/narucivanje/MainActivity.java
+++ app/src/main/java/ika/narucivanje/MainActivity.java
@@ -1,14 +1,56 @@
 package ika.narucivanje;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "activity-lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(LOG_TAG, "onSaveInstanceState() called");
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "OnStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "OnResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "OnPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "OnStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "OnDestroy() called");
     }
 }
