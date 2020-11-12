ac14709abf2d79b0a485f3e9ae6990a251945659
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-11-12 22:23:38 +0100

    Running app: MyViewModel promenjen

--- Running app/src/main/java/ika/test/runningapp/MyViewModel.java
+++ Running app/src/main/java/ika/test/runningapp/MyViewModel.java
@@ -9,28 +9,49 @@ import androidx.lifecycle.ViewModel;
 public class MyViewModel extends ViewModel {
 
     public static final String CALORIES_BURNED_KEY = "calories-burned";
+    public static final String CALORIES_NEEDED_KEY = "calories-needed";
 
-    private final MutableLiveData<Integer> caloriesBurned = new MutableLiveData<>();
-    private  boolean caloriesBurnedValied = false;
-    //private int caloriesBUrned = -1;
+    private final MutableLiveData<Integer> caloriesBurned = new MutableLiveData<>(-1);
+    private final MutableLiveData<Integer> caloriesNeeded = new MutableLiveData<>(-1);
+
+    private  boolean dataValid = false;
 
     public LiveData<Integer> getCaloriesBurned() {
         return caloriesBurned;
     }
 
+    public LiveData<Integer> getCaloriesNeeded() {
+        return caloriesNeeded;
+    }
+
     public void setCaloriesBurned(int value) {
-        caloriesBurnedValied = true;
+        dataValid = true;
         caloriesBurned.setValue(value);
     }
 
     public void initByBundle(Bundle bundle){
         if(bundle != null){
-            if(!caloriesBurnedValied){
+            if(!dataValid){
                 if(bundle.containsKey(CALORIES_BURNED_KEY)){
-                    caloriesBurnedValied = true;
+                    dataValid = true;
                     caloriesBurned.setValue(bundle.getInt(CALORIES_BURNED_KEY));
                 }
             }
         }
+    }
+
+    public void updateValues(double weight, double height, int age, boolean isMale, double duration, double met){
+
+        double caloriesNeededValue = 0;
+
+        if(isMale){
+            caloriesNeededValue = (int) 66 + 13.7 * weight + 5 * height - 6.8 * age;
+        }else{
+            caloriesNeededValue = (int) 655 + 9.6 * weight + 1.9 * height - 4.7 * age;
+        }
+        double caloriesBurnedValue = duration * met * 3.5 * weight / 200;
+
+        caloriesBurned.setValue((int) caloriesBurnedValue);
+        caloriesNeeded.setValue((int) caloriesNeededValue);
     }
 }
