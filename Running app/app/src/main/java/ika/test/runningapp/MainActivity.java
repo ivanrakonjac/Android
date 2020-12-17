5529064b9d907bf3576b4397da38024daf29f63f
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-12-17 23:26:13 +0100

    Running app: All logic for BottomNavigation moved from MainActivity.java to BottomNavigationUtil.java

--- Running app/app/src/main/java/ika/test/runningapp/MainActivity.java
+++ Running app/app/src/main/java/ika/test/runningapp/MainActivity.java
@@ -1,5 +1,6 @@
 package ika.test.runningapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
@@ -17,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if(savedInstanceState == null){
+            setupBottomNavigation();
+        }
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation(){
+        int[] navResourceIds = new int[]{
+                R.navigation.nav_graph_routes,
+                R.navigation.nav_graph_workouts,
+                R.navigation.nav_graph_calories
+        };
+
+        BottomNavigationUtil.setup(
+                binding.bottomNavigationView,
+                getSupportFragmentManager(),
+                navResourceIds,
+                R.id.nav_host_container
+        );
     }
 }
