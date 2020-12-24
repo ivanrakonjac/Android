5920a9a5099788c44c6a3dc0dd491db7db3fa5b4
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-12-24 23:27:57 +0100

    Running app: Add @HiltAndroidApp annotation and Hilt dependencies

---
+++ Running app/app/src/main/java/ika/test/runningapp/RunApplication.java
@@ -0,0 +1,9 @@
+package ika.test.runningapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class RunApplication extends Application {
+}
