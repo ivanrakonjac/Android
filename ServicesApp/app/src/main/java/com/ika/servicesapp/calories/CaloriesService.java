7fb63f9149a8870daa299cb193b56e1bde4e27bc
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-26 21:41:05 +0100

    ServicesApp: CaloriesService is moved to Foreground and Notification is added

--- ServicesApp/app/src/main/java/com/ika/servicesapp/calories/CaloriesService.java
+++ ServicesApp/app/src/main/java/com/ika/servicesapp/calories/CaloriesService.java
@@ -1,12 +1,22 @@
 package com.ika.servicesapp.calories;
 
+import android.app.Notification;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.media.app.NotificationCompat;
+
+import com.ika.servicesapp.R;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -39,6 +49,11 @@ public class CaloriesService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("SERVICES", "onStartCommand");
+
+        //NotificationManagerCompat.from(this).notify(1, getNotification());
+        createNotificationChannel();
+        startForeground(1, getNotification());
+
         if(!serviceStarted){
             scheduleTimer();
         }
@@ -56,5 +71,23 @@ public class CaloriesService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void createNotificationChannel(){
+        NotificationChannelCompat notificationChannel = new NotificationChannelCompat
+                .Builder("ServiceAppNotChannel",NotificationManagerCompat.IMPORTANCE_DEFAULT)
+                .setName("CHANNEL_MAIN")
+                .build();
+        NotificationManagerCompat.from(this).createNotificationChannel(notificationChannel);
+    }
+
+    private Notification getNotification(){
+        return new androidx.core.app.NotificationCompat.Builder(this,"ServiceAppNotChannel")
+                                                .setSmallIcon(R.drawable.baseline_bar_chart_black_18)
+                                                .setContentTitle("Naslov")
+                                                .setContentText("Content text")
+                                                .setColorized(true)
+                                                .setColor(ContextCompat.getColor(this,R.color.teal_200))
+                                                .build();
     }
 }
