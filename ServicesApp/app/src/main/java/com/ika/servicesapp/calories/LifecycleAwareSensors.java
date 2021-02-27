d25b096e8ccb9206d9b7dfe7a5a0368c94a780e5
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-27 18:00:42 +0100

    ServicesApp: How to use ambient temperature sensor

--- ServicesApp/app/src/main/java/com/ika/servicesapp/calories/LifecycleAwareSensors.java
+++ ServicesApp/app/src/main/java/com/ika/servicesapp/calories/LifecycleAwareSensors.java
@@ -2,10 +2,14 @@ package com.ika.servicesapp.calories;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import com.ika.servicesapp.MainActivity;
 
@@ -17,6 +21,17 @@ public class LifecycleAwareSensors implements DefaultLifecycleObserver {
 
     private  SensorManager sensorManager = null;
 
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.d("SENSOR_LOG_TAG", "temp_sensor:" + event.values[0]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
     @Inject
     public LifecycleAwareSensors() {
 
@@ -25,10 +40,19 @@ public class LifecycleAwareSensors implements DefaultLifecycleObserver {
     public void startMeasuring(Context context){
        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
 
-        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        /*List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
         for (Sensor sensor : sensorList) {
             Log.d("SENSOR_LOG_TAG", sensor.toString());
-        }
+        }*/
+
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    @Override
+    public void onDestroy(@NonNull LifecycleOwner owner) {
+        if(sensorManager != null){
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+    }
 }
